4a6474d99a073294ffb4e648c2b67e315e000a99
itsf4llofstars <irooted4hal@mailfence.com>
2022-03-10 10:42:50 -0600

    Instantiate main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import secrets
+
+
+def main():
+    """main function"""
+    pass
+
+
+if __name__ == "__main__":
+    from os import system
+    from sys import exit
+
+    system("clear")
+    exit(main())
