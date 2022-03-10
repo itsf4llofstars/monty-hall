b272bef21333507a865eb1b83d7b5ef76df25485
itsf4llofstars <irooted4hal@mailfence.com>
2022-03-10 10:46:07 -0600

    Add a quick test of script functionality

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
+"""main.py"""
 import secrets
+
+
+def say_hi(the_name) -> str:
+    return f"Hello {the_name.title()}"
 
 
 def main():
     """main function"""
-    pass
+    name: str = "darren"
+    greeting: str = say_hi(name)
+    print(greeting)
 
 
 if __name__ == "__main__":
