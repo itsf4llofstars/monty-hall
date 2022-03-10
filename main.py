e56208d75f1221f290e583c4a8a211768b82921a
itsf4llofstars <irooted4hal@mailfence.com>
2022-03-10 11:06:42 -0600

    Add get_winning_door funtion
    Function added, choosing 1 win door, 2 lose doors, shuffles doors 1000
    times

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python3
 """main.py"""
+import random
 import secrets
 
+# GLOBALS
+WIN_LOSS = [False, True]
+DOORS = [None, None, None]
 
-def say_hi(the_name) -> str:
-    return f"Hello {the_name.title()}"
+
+def set_winning_door():
+    door = secrets.randbelow(3)
+    DOORS[door] = True
+    if DOORS[0] is not None:
+        DOORS[1] = not DOORS[0]
+        DOORS[2] = not DOORS[0]
+    elif DOORS[1] is not None:
+        DOORS[0] = not DOORS[1]
+        DOORS[2] = not DOORS[1]
+    elif DOORS[2] is not None:
+        DOORS[0] = not DOORS[2]
+        DOORS[1] = not DOORS[2]
+
+    print(DOORS)
+    for _ in range(1000):
+        random.shuffle(DOORS)
+    print(DOORS)
 
 
 def main():
     """main function"""
-    name: str = "darren"
-    greeting: str = say_hi(name)
-    print(greeting)
+    set_winning_door()
 
 
 if __name__ == "__main__":
